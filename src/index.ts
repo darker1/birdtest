e3f0977912c4e5f9b8969a75ef15387b7426737f
Kyle Wall <kyle.wall@hyperloop-one.com>
2018-10-08 18:25:02 -0700

    clean up index

--- src/index.ts
+++ src/index.ts
@@ -6,23 +6,26 @@ import { GreatestTotalDistanceCommand } from "./commands/greatest_total_distance
 import { HighestPayerCommand } from "./commands/highest_payer";
 import { LongestBirdWaitTimeCommand } from "./commands/longest_bird_wait_time";
 import { AverageSpeedCommand } from "./commands/average_speed";
+import { Command } from "./commands/command";
 
 const application = async () => {
   const events = await EventParser.parseFile('events.txt');
+  
   const eventMaps = new EventMaps(events);
-  const totalBirds = new TotalBirdsCommand(eventMaps);
-  const farthestFromDrop = new FarthestFromDropPointCommand(eventMaps);
-  const farthestFromStartLocation = eventMaps.birdFarthestFromDropPoint();
-  const greatestTotalDistance = new GreatestTotalDistanceCommand(eventMaps);
-  const highestPayer = new HighestPayerCommand(eventMaps);
-  const longestWaitTime = new LongestBirdWaitTimeCommand(eventMaps);
-  const averageSpeed = new AverageSpeedCommand(eventMaps);
-  console.log(totalBirds.run());
-  console.log(farthestFromDrop.run());
-  console.log(greatestTotalDistance.run());
-  console.log(highestPayer.run());
-  console.log(longestWaitTime.run());
-  console.log(averageSpeed.run());
+  const commands = [
+    new TotalBirdsCommand(eventMaps),
+    new FarthestFromDropPointCommand(eventMaps),
+    new GreatestTotalDistanceCommand(eventMaps),
+    new HighestPayerCommand(eventMaps),
+    new LongestBirdWaitTimeCommand(eventMaps),
+    new AverageSpeedCommand(eventMaps)
+  ];
+
+  console.log('****************************');
+  console.log('*  Bird Test by Kyle Wall  *')
+  console.log('****************************');
+
+  commands.forEach(c => console.log(c.run()));
 }
 
 application();
